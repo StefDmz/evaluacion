a1af70bee096906cb4fbf1c5aab2bb6aa73000e7
StefDmz <andrea.rosales@laureate.mx>
2024-10-10 23:11:19 -0600

    add categories service and mod backend for cors

---
+++ Frontend/src/app/core/services/categories/categories.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from '../../interfaces/category.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+  private baseUrl: string = environment.baseUrl;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getCategories(): Observable<Category[]>{
+    return this.http.get<Category[]>(`${ this.baseUrl }/categories`);
+  }
+}
