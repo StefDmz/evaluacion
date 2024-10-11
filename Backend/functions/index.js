e31cd23bf5e15af46954fd1d311cffb3a73c18f2
StefDmz <andrea.rosales@laureate.mx>
2024-10-11 10:51:16 -0600

    Add orders controllers

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -13,9 +13,13 @@ app.use(express.json());
 const products = require('./controller/products.js');
 const categories = require('./controller/categories.js');
 const schedules = require('./controller/schedules.js');
+const orders = require('./controller/orders.js');
+const ordersDetail = require('./controller/order-details.js');
 
 app.use('/products', products);
 app.use('/categories', categories);
 app.use('/schedules', schedules);
+app.use('/orders', orders);
+app.use('/order-details', ordersDetail);
 
 exports.app = functions.https.onRequest(app);
