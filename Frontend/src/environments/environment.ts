46b3cb5c4aa22b27877c03030c49baded1203726
StefDmz <andrea.rosales@laureate.mx>
2024-11-04 19:24:56 -0600

    Add Render URL for backend

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     // baseUrl: 'http://localhost:5000/pastesapp-da6ac/us-central1/app'
-    baseUrl: 'http://localhost:3000'
+    // baseUrl: 'http://localhost:3000'
+    // baseUrl: 'https://grupoquack.com.mx/pastes/assets/data/data.json'
+    baseUrl: 'https://all-data-ibx6.onrender.com'
 }
