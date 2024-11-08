0a09b09fff3237416d733c32e8d137e6baf550e4
StefDmz <andrea.rosales@laureate.mx>
2024-11-08 12:42:51 -0600

    Add category form

--- Frontend/src/app/modules/categories/categories.module.ts
+++ Frontend/src/app/modules/categories/categories.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoriesTableComponent } from './components/categories-table/categories-table.component';
 import { SharedModule } from "../../shared/shared.module";
+import { CategoryFormComponent } from './components/category-form/category-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    CategoriesTableComponent
+    CategoriesTableComponent,
+    CategoryFormComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
 ], exports: [
-    CategoriesTableComponent
+    CategoriesTableComponent,
+    CategoryFormComponent,
   ]
 })
 export class CategoriesModule { }
